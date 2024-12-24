81dcb232a14afd8818ca8d30dac117f20ae41344
aman mulani <mulaniaman0504@gmail.com>
2024-12-24 21:28:17 +0530

    fix:initial Scaffolding

---
+++ src/Wrapper.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Wrapper = () => {
+  return (
+    <View>
+      <Text>Wrapper</Text>
+    </View>
+  );
+};
+
+export default Wrapper;
+
+const styles = StyleSheet.create({});

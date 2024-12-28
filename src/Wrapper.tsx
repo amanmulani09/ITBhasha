dbc882313d45e3740a2b923279e4c69d48c5c5e6
aman mulani <mulaniaman0504@gmail.com>
2024-12-28 15:24:06 +0530

    fix:added redux toolkit for state management

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -2,11 +2,16 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigation from "./navigation";
+import { Provider } from "react-redux";
+import store from "./store";
+
 const Wrapper = () => {
   return (
-    <NavigationContainer>
-      <AppNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigation />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,7 +1,11 @@
 import TabNavigation from "./tab";
 import AuthStack from "./stack-navigation/auth";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/index";
 const AppNavigation = () => {
-  const isAuthenticated = true;
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isLoggedIn
+  );
   return isAuthenticated ? <TabNavigation /> : <AuthStack />;
 };
 
